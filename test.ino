58dd20008611c531ca3d2ee0cd0f4fbb67a7d62e
Alex H <alexander707huh@gmail.com>
2016-03-30 18:18:13 -0400

    Added files via upload
    Arduino servo code:
    goes from minimum to maximum

---
+++ test.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+ Servo servo1; 
+void setup() {
+  // put your setup code here, to run once:
+
+  servo1.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i <= 180; i++){
+    delay(20);
+    servo1.write(i);
+  }
+  for(int i = 180; i >= 0; i--){
+    delay(20);
+    servo1.write(i);
+  }
+
+}
